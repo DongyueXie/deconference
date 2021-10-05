5ee8c654c720b90c99cac8703865ae9ea0ea1d81
DongyueXie <dongyxie@gmail.com>
2021-10-05 08:40:23 -0500

    neuron revise

---
+++ code/real/real_manuscript.R
@@ -0,0 +1,94 @@
+
+library(MuSiC)
+library(Matrix)
+sce_to_es = function(sce){
+  #bulk.data = ExpressionSet(assayData = counts(enge_bulk$bulk))
+  pheno_Data = data.frame(cell_type = sce$cell_type,individual = sce$individual)
+  rownames(pheno_Data) = colnames(sce)
+  ref.data = ExpressionSet(assayData = as.matrix(counts(sce)),phenoData = AnnotatedDataFrame(pheno_Data))
+  ref.data
+}
+
+# For neuron dataset,
+indis_ref = readRDS('data/neuron/indis_ref_12400by6by97.rds')
+day30sce = readRDS('data/neuron/day30sce.rds')
+
+
+
+p1 = c(0.1,0.1,0.15,0.15,0.2,0.3)
+p2 = c(0.1,0.15,0.25,0.3,0.1,0.1)
+
+neuron_simu_study = function(indis_ref,day30sce,
+                             p1,p2,p1.prop=0.5,
+                             bulk_lib_size = 500,
+                             n_ref=11,n_rep=1,seed=12345,
+                             verbose=FALSE,
+                             R01=NULL){
+  gene_to_use = dimnames(indis_ref)[[1]]
+  individuals = dimnames(indis_ref)[[3]]
+  celltypes = dimnames(indis_ref)[[2]]
+  n_indi = length(individuals)
+  G = length(gene_to_use)
+  K = length(p1)
+  n_bulk = n_indi - n_ref
+
+  # generate bulk true cell proportions
+  n.p1.temp =  round(n_bulk*p1.prop)
+  groups = c(rep(1,n.p1.temp),rep(2,n_bulk-n.p1.temp))
+  b = cbind(p1%*%t(rep(1,n.p1.temp)),p2%*%t(rep(1,n_bulk-n.p1.temp)))
+
+
+  set.seed(seed)
+
+  for(r in 1:n_rep){
+    # randomly draw reference individuals
+
+    ref.idx = sample(1:n_indi,n_ref)
+
+
+    X_array_ref = indis_ref[,,ref.idx]
+    X_array_bulk = indis_ref[,,-ref.idx]
+
+    # create bulk data
+
+    perm.temp = sample(1:n_bulk)
+    groups = groups[perm.temp]
+    b = b[,perm.temp]
+
+    mb = lapply(1:n_bulk,function(i){X_array_bulk[,,i]%*%b[,i]})
+    mb = do.call(cbind,mb)
+    thetab = apply(mb,2,function(z){z/sum(z)})
+    y = matrix(rpois(G*n_bulk,bulk_lib_size*G*thetab),nrow=G)
+    rownames(y) = gene_to_use
+    colnames(y) = individuals[-ref.idx]
+
+    # fit our model
+    X = apply(X_array_ref,c(1,2),mean,na.rm=TRUE)
+    V = t(apply(X_array_ref,c(1),function(z){(cov(t(z),use = 'complete.obs'))}))/n_ref
+    fit.vash = vashr::vash(sqrt(rowSums(V)),df=n_ref-1)
+    w = 1/(fit.vash$sd.post)^2
+
+    fit.err.cor.weight.cv = estimation_func2(y=y,
+                                             X=X,
+                                             Vg=V,
+                                             w=w,
+                                             hc.type='jackknife_indep',
+                                             verbose=verbose,
+                                             R01=R01)
+
+    # fit music
+    # create bulk expression set object
+
+    phenoData = data.frame(SubjectName = individuals[-ref.idx])
+    rownames(phenoData) = colnames(y)
+    bulk_es = ExpressionSet(assayData = y,phenoData = AnnotatedDataFrame(phenoData))
+
+    # create reference expression set object
+    ref_sce = day30sce[gene_to_use,day30sce$individual%in%(individuals[ref.idx])]
+    ref_sce$individual = droplevels(ref_sce$individual)
+    ref_es = sce_to_es(ref_sce)
+    fit_music = music_prop(bulk_es,ref_es,clusters='cell_type ',samples='individual',select.ct = celltypes)
+
+  }
+
+}
